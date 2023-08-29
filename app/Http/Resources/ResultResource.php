ac14563375000693902858d846e5b6758ca04f30
Emilson Filho <emilsonfilhocontato@gmail.com>
2023-08-29 15:16:32 -0300

    Upando ResultResource

---
+++ app/Http/Resources/ResultResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ResultResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'resultado_id' => $this->resultado_id,
+            'resultado_nome' => $this->resultado_nome,
+        ];
+    }
+}
